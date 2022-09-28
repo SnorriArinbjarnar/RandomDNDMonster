ad80fb61c91756778f51c33e28b34a511c96584a
Snorri Arinbjarnar <snorriarinbjarnar@gmail.com>
2022-09-28 15:38:20 +0000

    changing code to reading deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 
+const path = require('path');
 const express = require('express');
 const axios = require('axios');
 const { getRandomMonsterByUrl, getAbilityMod } = require('./utils/helpers');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+/* 
+    Have Node serve the files for our built React app
+*/
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 /* 
    DND Rest API used: https://api.open5e.com/
@@ -96,6 +101,13 @@ app.get("/api/monster/:type/:cr", async(req, res) => {
         res.json(monster)
     }
     
+});
+
+/* 
+    All other GET requests not handled before will return our React app
+*/
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
